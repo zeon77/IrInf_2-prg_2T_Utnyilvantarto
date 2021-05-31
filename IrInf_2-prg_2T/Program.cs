1a4d97f7a983435cc47ea211d8fefed62b166e9d
zeon77 <zeon77@gmail.com>
2021-05-31 09:08:27 +0200

    Add project files.

---
+++ IrInf_2-prg_2T/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrInf_2_prg_2T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
